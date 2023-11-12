ac432266f681f32096f36788a1c8beb0057f3c34
Błażej Niewiadomski <blaz.nie@protonmail.com>
2023-11-12 18:47:05 +0100

    overhauled ui

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 
 const interpreter_url = "http://127.0.0.1:8081/index.php"
 let input_line = document.querySelector("#repl-input")
-let submit_button = document.querySelector("#run-button")
+
+let replHistory = []
+
+function handleKeyPress(event) {
+  switch (event.code) {
+    case "Enter":
+      //submit code
+      submitExpr(event.target)
+      break;
+    case "KeyL":
+      if (event.ctrlKey) {
+        event.preventDefault()
+        clearRepl(event.target)
+        console.log("clearing console")
+      }
+      break;
+    default:
+      return 0
+  }
+}
+
+async function submitExpr(input) {
+  let expression = input.value
+
+  input_line.value = "";
+  let results = { return_value: 20, stdout_output: "hewwo" }//await sendCode(expression)
+  let history = document.querySelector("#repl-history")
+  let new_history_entry = createHistoryEntry(`  Lisp> ${expression}`,
+                                             results.return_value,
+                                             results.stdout_output)
+  history.appendChild(new_history_entry)
+}
+
+function clearRepl(event) {
+  let repl_frame = document.querySelector("#repl-frame")
+
+  // input_line.scroll(0, 30)
+  window.scroll(0, -700)
 
 
-function createHistoryEntry(code, returnValue, evalResults) {
+}
+
+
+
+function createHistoryEntry(expr, return_value, eval_results) {
   let new_history_entry = document.createElement("div")
-  let expression_div = document.createElement("div")
-  let return_value_div = document.createElement("div")
-  let stdout_div = document.createElement("div")
-  expression_div.innerHTML = code
-  return_value_div.innerHTML = returnValue
-  stdout_div.innerHTML = evalResults
+  let expression_div    = document.createElement("div")
+  let return_value_div  = document.createElement("div")
+  let stdout_div        = document.createElement("div")
+
+  replHistory.push({
+    expression: expr,
+    return_value: return_value,
+    eval_results: eval_results
+  })
+
+  expression_div.innerHTML   = expr
+  return_value_div.innerHTML = return_value
+  stdout_div.innerHTML       = eval_results
 
   new_history_entry.append(expression_div, return_value_div, stdout_div)
   //add containers for all three values
@@ -20,19 +68,6 @@ function createHistoryEntry(code, returnValue, evalResults) {
 }
 
 
-async function onCodeSubmit(event) {
-  let expression = event.target.value
-  if (event.code == "Enter") {
-
-    input_line.value = "";
-    let results = await sendCode(expression)
-    let history = document.querySelector("#repl-history")
-    let new_history_entry = createHistoryEntry(expression,
-                                               results.return_value,
-                                               results.stdout_output)
-    history.appendChild(new_history_entry)
-  }
-}
 
 async function sendCode(code) {
   try {
@@ -53,4 +88,4 @@ async function sendCode(code) {
 
 
 
-input_line.addEventListener("keydown", onCodeSubmit)
+input_line.addEventListener("keydown", handleKeyPress)
